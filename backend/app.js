9482e1033c8f189dc33a473e8b044dbde6ef7a8f
AM <malerischam@yahoo.com>
2024-12-17 03:14:50 +0400

    adding frontend and css

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const rateLimit = require("./node_modules/express-rate-limit/dist/index.d.cts");
+require('dotenv').config();
+const axios = require('axios');
+const rateLimit = require("express-rate-limit");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const API_URL = 'https://v6.exchangerate-api.com/v6';
+const API_KEY = process.env.EXCHANGE_RATE_API_KEY
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -14,10 +18,27 @@ app.use(apiLimiter);
 
 //Conversion
 app.post("/api/convert", async (req, res) => {
-  const data = req.body;
-  console.log(data);
+  const {from, to, amount} = req.body;
+  console.log({from, to, amount});
+
+  //Construct the api
+  const url = `${API_URL}/${API_KEY}/pair/${from}/${to}/${amount}`;
+  const response = await axios.get(url);
+   if(response.data && response.data.result === 'success') {
+    res.json({
+      base:from,
+      target:to,
+      conversionRate: response.data.conversion_rate,
+      convertedAmount: response.data.conversion_result,
+
+    });
+   } else {
+    res.json({message: "Error converting currency", details: response.data})
+   }
   try {
-  } catch (error) {}
+  } catch (error) {
+    res.json({message: 'Error converting currency', details: response.message})
+  }
 });
 
 // Start the server
